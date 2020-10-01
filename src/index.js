9f0e3bd6a128a2322e36d8a4977170f75f6d77e4
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-01 23:01:11 +0500

    feat: add solution for min

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ exports.min = function min(array) {
     array.length < 1
   )
     return 0;
+  const qSort = new QuickSort();
+  return qSort.sort(array, 0, array.length - 1)[0];
 };
 
 exports.max = function max(array) {
@@ -22,3 +24,45 @@ exports.avg = function avg(array) {
     return 0;
   return array.reduce((current, prev) => current + prev) / array.length;
 };
+
+class QuickSort {
+  swap(items, leftIndex, rightIndex) {
+    let temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+  }
+  partition(items, left, right) {
+    // const random = Math.floor(Math.random() * (high + 1));
+    const pivot = items[Math.floor((left + right) / 2)];
+    let i = left;
+    let j = right;
+    while (i <= j) {
+      while (items[i] < pivot) {
+        i++;
+      }
+      while (items[j] > pivot) {
+        j--;
+      }
+      if (i <= j) {
+        this.swap(items, i, j);
+        i++;
+        j--;
+      }
+    }
+    return i;
+  }
+
+  sort(items, left, right) {
+    let index;
+    if (items.length > 1) {
+      index = this.partition(items, left, right);
+      if (left < index - 1) {
+        this.sort(items, left, index - 1);
+      }
+      if (index < right) {
+        this.sort(items, index, right);
+      }
+    }
+    return items;
+  }
+}
