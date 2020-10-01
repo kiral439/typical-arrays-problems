36969f6159ef59afdf243c9e7b16e9617bd914d7
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-01 21:37:12 +0500

    feat: add solution of avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+  if (
+    typeof array === "undefined" ||
+    array === null ||
+    array.length === null ||
+    array.length < 1
+  )
+    return 0;
+};
 
-exports.min = function min (array) {
+exports.max = function max(array) {
   return 0;
-}
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (
+    typeof array === "undefined" ||
+    array === null ||
+    array.length === null ||
+    array.length < 1
+  )
+    return 0;
+  return array.reduce((current, prev) => current + prev) / array.length;
+};
